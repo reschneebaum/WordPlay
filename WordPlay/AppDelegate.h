295c9120d48e3844814225422b97ead6879921d6
Mobile Maker <mobilemaker@MM-Moderator-Mac-mini.local>
2015-07-14 13:41:05 -0500

    Initial Commit

---
+++ WordPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPlay
+//
+//  Created by Mobile Maker on 7/14/15.
+//  Copyright (c) 2015 Mobile Maker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordPlay
+//
+//  Created by Mobile Maker on 7/14/15.
+//  Copyright (c) 2015 Mobile Maker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
