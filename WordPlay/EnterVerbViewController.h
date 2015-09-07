805ea73e13a011b720e46ff7a7593c7266b29494
Rachel Schneebaum <reschneebaum@gmail.com>
2015-09-07 18:07:59 -0500

    add files

---
+++ WordPlay/EnterVerbViewController.h
@@ -0,0 +1,17 @@
+//
+//  EnterVerbViewController.h
+//  WordPlay
+//
+//  Created by Rachel Schneebaum on 7/14/15.
+//  Copyright (c) 2015 Mobile Maker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterVerbViewController : UIViewController
+
+@property NSString *name;
+@property NSString *adjective;
+@property NSString *noun;
+
+@end
